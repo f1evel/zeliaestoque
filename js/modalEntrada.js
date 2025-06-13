7ed8c7e456e147b98f07ca3c0abef790fb3e0353
f1evel <dyegopimenta@gmail.com>
2025-06-13 10:44:19 -0300

    Add month adjustment for entrada vencimento

--- js/modalEntrada.js
+++ js/modalEntrada.js
@@ -23,6 +23,7 @@ export function abrirModalEntrada(produto) {
   document.getElementById("entrada-forma-pagamento").value = "pix";
   document.getElementById("entrada-observacoes").value = "";
   const dataEntrada = new Date(produtoCadastroAtual.dataEntrada);
+  dataEntrada.setMonth(dataEntrada.getMonth() + 1);
   document.getElementById("entrada-primeiro-vencimento").value = dataEntrada.toISOString().split("T")[0];
   document.getElementById("modal-entrada").style.display = "block";
   document.getElementById("fundo-modal").style.display = "block";
